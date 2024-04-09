ce24aa26a6a2af7479dae5aca2c16efc8eae796b
bolivaraponte <apontebolivar@gmail.com>
2024-04-09 00:34:48 -0500

    dbRDA analyses completed. New plots for LBI, dbRDAs, Hill orders.

--- scripts_notes/mim3_rmdup_slurm.sh
+++ scripts_notes/mim3_rmdup_slurm.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#SBATCH --qos=long
+#SBATCH --qos=normal
 #SBATCH --job-name MIM3_ddRAD_rmdup
 #SBATCH --error /lustre/project/svanbael/bolivar/Mimulus_sequences/mim3_bioinformatics/MIM3_ddRAD_rmdup.error     
 #SBATCH --output /lustre/project/svanbael/bolivar/Mimulus_sequences/mim3_bioinformatics/MIM3_ddRAD_rmdup.output  
-#SBATCH --time=2-00:00:00
+#SBATCH --time=23:00:00
 #SBATCH --mem=128000 #Up to 256000, the maximum increases queue time
 #SBATCH --nodes=1               # One is enough. When running MPIs, anywhere from 2-4 should be good.
 #SBATCH --ntasks-per-node=1    # Number of Tasks per Node (MPI processes)
 #SBATCH --cpus-per-task=20      # Number of cpu-cores (threads) per task (OMP threads)
+#SBATCH --array=24,26,28,29,31,32,33,34,35,36,42  # Array of IDs
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=baponterolon@tulane.edu
 
@@ -25,11 +26,15 @@ echo job id: $SLURM_JOB_ID
 echo Number of tasks: $SLURM_NTASKS
 
 #Run Python script
-filePrefix=("BAR24" "BAR26" "BAR28" "BAR29" "BAR31" "BAR32" "BAR33" "BAR34" "BAR35" "BAR36" "BAR42")
+# filePrefix=("BAR24" "BAR26" "BAR28" "BAR29" "BAR31" "BAR32" "BAR33" "BAR34" "BAR35" "BAR36" "BAR42")
+# 
+# for prefix in "${filePrefix[@]}"; do
+#     python rmdup_molbarcodes.py -p "$prefix" -s fastq.gz
+# done
 
-for prefix in "${filePrefix[@]}"; do
-    python rmdup_molbarcodes.py -p "$prefix" -s fastq.gz
-done
+# Array job
+sh ./bar$(printf "%02d" $SLURM_ARRAY_TASK_ID).sh
+
 module purge
 
 echo End Job
