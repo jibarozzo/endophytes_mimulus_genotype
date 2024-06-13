3b4c0e9ea440939d9402d1f89b878272e73da1ae
jibarozzo <bolaponte@proton.me>
2024-06-13 09:02:30 -0500

    Making paths into variables for better reproducibility.

--- scripts_notes/ddRAD_pipeline/Step1_readQC.sh
+++ scripts_notes/ddRAD_pipeline/Step1_readQC.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 #SBATCH --job-name=fastq
 #SBATCH --mail-type=ALL # Valid values: BEGIN, END, FAIL, REQUEUE and ALL.
-#SBATCH --mail-user=cdong1@tulane.edu
+#SBATCH --mail-user=baponterolon@tulane.edu
 #SBATCH --output=/lustre/project/svanbael/bolivar/Mimulus_sequences/mim3_bioinformatics/ddRAD/2_fastQC/fastQC_files/fastqc.out
 #SBATCH --error=/lustre/project/svanbael/bolivar/Mimulus_sequences/mim3_bioinformatics/ddRAD/2_fastQC/fastQC_files/fastqc.err
 #SBATCH --time=0-24:00:00
+#SBATCH --partition=centos7
 #SBATCH --cpus-per-task=20  #: Cpus per Task
 #SBATCH --nodes=1            #: Number of Nodes
-#SBATCH --array=1-45  # Job array (1-n) when n is number of unique samples that came off the sequencer
+
 
 #### load modules###
 module load fastqc
 module load gnuparallel
-module load anaconda3
-unset PYTHONPATH
-source activate /lustre/project/kferris/conda/conda-envs/qc_env # This is to load a virtual environment from which to load 'multiqc'
+module load anaconda3/2023.07
+#unset PYTHONPATH
+
+
+conda activate py310 # This is to load a virtual environment (py310) from which to load 'multiqc'
 ###########################################################################################
 <<Note_toUser
+
+Modified by BAR 2024-06-12
 The script below serves to perform most of the QC steps needed before aligning and 
 variant calling.
+In a previous version the SLURM header ran as an array job. This is not necessary because we are running a `for` loop in the script.
 
 Softlinks to files only need to be made once and it's best not to work with the original 
 fastq or fastq.gz files. 
@@ -32,7 +38,10 @@ echo "Start Job"
 
 ### Create folders fastQC_files and multiqc in 2_fastQC FIRST
 ### set working directory (file output directory) ###
-cd /lustre/project/svanbael/bolivar/Mimulus_sequences/mim3_bioinformatics/ddRAD/2_fastQC/
+### Variables for the fastq working directory and the input files directory ###
+WD="/lustre/project/svanbael/bolivar/Mimulus_sequences/mim3_bioinformatics/ddRAD/2_fastQC/"
+INPUT="/lustre/project/svanbael/bolivar/Mimulus_sequences/mim3_bioinformatics/ddRAD/1_data/"
+cd $WD
 
 ### creating softlinks for fastq files to current directory ###
 ln -s =/lustre/project/svanbael/bolivar/Mimulus_sequences/mim3_bioinformatics/ddRAD/1_data/* .
@@ -50,7 +59,7 @@ multiqc . -o /lustre/project/svanbael/bolivar/Mimulus_sequences/mim3_bioinformat
 echo "End MultiQC"
 
 ### Unsetting environmentc ###
-source deactivate
+conda deactivate
 module purge #unloads all module
 
 echo "End Job"
